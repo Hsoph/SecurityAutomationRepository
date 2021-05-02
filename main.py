7e4105ce3588f42a840d6b59d8f1f253f4ac6731
Hsoph <83501431+Hsoph@users.noreply.github.com>
2021-05-02 18:52:11 -0400

    Update
    Version 2.0

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import random
-
-password_characters = '!@#$%^&*()-=_+`~[]{]\|;:,<.>/?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-
 print("You past passwords may have been compromised.  Generate something stronger below.")
-
-passwordLength = input('Requested length: ')
-passwordLength = int(passwordLength)
-
+password_characters = '!@#$%^&*()-=_+`~[]{]\|;:,<.>/?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+lengthofpassword = input('Requested length: ')
+lengthofpassword = int(lengthofpassword)
 print('See below: ')
-
 passwordtextdocument = open('supersecurepassworddocument.txt', 'w')
-for p in range(passwordLength):
+for p in range(lengthofpassword):
     password = ''
-    for c in range(passwordLength):
+    for c in range(lengthofpassword):
         password = ""
         password += random.choice(password_characters)
     print (password)
     passwordtextdocument.write(password)
 passwordtextdocument.close()
-
 print('There you go!')
